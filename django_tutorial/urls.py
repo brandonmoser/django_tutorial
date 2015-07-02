4452330c429c8119c1322d377076d1647c5e22b6
Brandon Moser <brandon@brandonmoser.com>
2015-07-02 12:32:07 -0500

    Renamed internal references from django_tutorial2 to django_tutorial

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -1,4 +1,4 @@
-"""django_tutorial2 URL Configuration
+"""django_tutorial URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
